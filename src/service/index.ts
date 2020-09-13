c819f0bafbb8d031b33156c89fe42e13bf1e249c
michaeleze <michaeleze3@gmail.com>
2020-09-14 01:16:56 +0200

    ...

--- src/service/index.ts
+++ src/service/index.ts
@@ -24,23 +24,23 @@ class ToDoService extends Observable {
 
   public async createNewTask(task: string) {
     const uniqueId = Math.floor(Math.random() * 20);
-    const body =  `{\"id\":\"${uniqueId}\",\"text\":\"${task}\"}`;
+    // const body =  `{\"id\":\"${uniqueId}\",\"text\":\"${task}\"}`;
 
-    await useFetch('http://localhost:3000/api/tasks', {body, method: 'POST'});
+    // await useFetch('http://localhost:3000/api/tasks', {body, method: 'POST'});
   };
 
   public async updateTask(id: string, text: string) {
-    const body = `{\"id\":\"${id}\",\"text\":\"${text}\"}`;
+    // const body = `{\"id\":\"${id}\",\"text\":\"${text}\"}`;
 
-   useFetch(`http://localhost:3000/api/tasks/${id}`, {body, method: 'PUT'})
-     .then(response => response && response.json())
-     .then(response => console.log(Object.values(response)))
+  //  useFetch(`http://localhost:3000/api/tasks/${id}`, {body, method: 'PUT'})
+  //    .then(response => response && response.json())
+  //    .then(response => console.log(Object.values(response)))
   };
 
   public async deleteTask(id: string, text: string) {
-    const body =  `{\"id\":\"${id}\",\"text\":\"${text}\"}`;
+    // const body =  `{\"id\":\"${id}\",\"text\":\"${text}\"}`;
 
-    await useFetch(`http://localhost:3000/api/tasks/${id}`, {body, method: 'DELETE'});
+    // await useFetch(`http://localhost:3000/api/tasks/${id}`, {body, method: 'DELETE'});
   };
 
   private getInstance() {

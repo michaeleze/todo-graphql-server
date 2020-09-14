a8cb2527d6b8b1bda04a2034583998f6c387f101
michaeleze <michaeleze3@gmail.com>
2020-09-14 08:21:04 +0200

    removed react package

--- index.js
+++ index.js
@@ -65,15 +65,11 @@ const resolvers = {
     }
 };
 
-const serverOptions = {
-    port: process.env.PORT || 5000,
-};
-
 const server = new GraphQLServer({
     typeDefs,
     resolvers
 });
 
-server.listen(serverOptions).then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
 });
